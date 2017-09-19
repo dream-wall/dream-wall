e7e6b7837e3a15489f56687e333ddd5140cbbd72
胡志甫 <banjuanshutong@126.com>
2017-09-19 15:05:12 +0800

    servie完善

--- server/routes/dream/index.js
+++ server/routes/dream/index.js
@@ -1,3 +1,6 @@
+/**
+ * Created by 胡志甫 on 2017/9/18.
+ */
 const dreamContoller=require('../../controllers/dream');
 
 module.exports = function(router){
--- server/service/index.js
+++ server/service/index.js
@@ -12,3 +12,33 @@ _service.findAndCountAll = async function (modelObj, options) {
     count:await modelObj.find(options.find_con).count()
   };
 }
+_service.findAll = async function (modelObj, condition) {
+  return await modelObj.find(condition);
+}
+
+_service.findOne = async function (modelObj, condition) {
+  return await modelObj.findOne(condition)
+}
+
+_service.findById = async function (modelObj, id) {
+  return await modelObj.findById(id);
+};
+_service.create = async function (modelObj, article) {
+  return await modelObj.create(article);
+};
+
+_service.update = async function (modelObj, setStatement, condition) {
+  return await modelObj.update(condition,setStatement,{ multi: true });
+}
+
+_service.deleteAll = async function (modelObj, condition) {
+  return await modelObj.remove(condition);
+}
+
+_service.insertAll = async function (modelObj, articles) {
+  return await modelObj.create(...articles);
+}
+
+_service.count= async function (modelObj, condition) {
+  return await modelObj.where(condition).count();
+}
